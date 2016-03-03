d8411180b27e59144cd1832072948ddf4094835b
Kosta Vasileiou <hello@vsl.io>
2016-03-03 16:58:54 +0000

    Added json-server, dummy db. API is working alright.

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 
 **This includes:**
 - [x] Express
+- [x] json-server
 - [x] Webpack
 - [x] React
 - [ ] Redux
@@ -19,26 +20,37 @@ Clone this [repo](https://github.com/vslio/the-dots-demo):
 ```
 git clone git@github.com:vslio/the-dots-demo.git /desired-folder
 ```
+
+Make sure you install JSON Server globally for the API server to work:
+```
+npm install -g json-server
+```
+*Note: You might need to prepend `sudo ` depending on the permissions of your user's node_modules folder.*
+
 Fire up the console, navigate to the cloned repo and install all the project's dependencies:
 ```
 cd /desired-folder/
 npm install
 ```
+
 Build the project and start development:
 ```
 npm start
 ```
 
-Success! The project is accessible under `http://localhost:3000`. Happy coding!
+Success! The project is accessible on `http://localhost:3000` and the API server on `http://localhost3001`.
 
 ##Structure
     .
     ├── client                  # Contains all the client files
     │   ├── components          # React components
-    │   ├── styles              # Core stylesheets (not component-specific)
+    │   └── styles              # Core stylesheets (not component-specific)
     ├── config                  # Webpack (for now) configuration files
-    ├── dist                    # Asset bundles (JS, images etc)
-    └── shared                  # Shared files between the server and the client-side
+    ├── db                      # Storing a mock DB file for the API
+    ├── dist                    # Final asset bundles (JS, images etc)
+    └── shared                  # Shared files between the server and the client
 
-##CSS
+
+##Things you need to know
+>CSS
 ...
--- server.development.js
+++ server.development.js
@@ -1,37 +1,52 @@
-const path = require('path');
-const express = require('express');
-const webpack = require('webpack');
-const webpackConfig = require('./config/webpack/webpack.config.development');
-const ProgressPlugin = require('webpack/lib/ProgressPlugin');
-const ProgressBar = require('progress');
-const server = express();
-const webpackCompiler = webpack(webpackConfig);
+/**
+ * Module requires
+ */
+const path = require('path')
+const express = require('express')
+const jsonServer = require('json-server')
+const webpack = require('webpack')
+const webpackConfig = require('./config/webpack/webpack.config.development')
+const ProgressPlugin = require('webpack/lib/ProgressPlugin')
+const ProgressBar = require('progress')
+
+/**
+ * Server constants declaration
+ */
+const server = express()
+
+/**
+ * Progressbar setup and initialisation
+ */
+const webpackCompiler = webpack(webpackConfig)
 const bar = new ProgressBar(':bar [:percent] :message', {
   total: 50
-});
+})
 
 webpackCompiler.apply(
   new ProgressPlugin((percentage, msg) => {
     bar.update(percentage, {
       message: msg
-    });
+    })
   })
-);
+)
 
+/**
+ * Server setup and initialisation
+ */
 server.use(require('webpack-dev-middleware')(webpackCompiler, {
   noInfo: true,
   publicPath: webpackConfig.output.publicPath
-}));
+}))
 
-server.use(require('webpack-hot-middleware')(webpackCompiler));
+server.use(require('webpack-hot-middleware')(webpackCompiler))
 
 server.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/index.html'));
-});
+  res.sendFile(path.join(__dirname, '/client/index.html'))
+})
 
 server.listen(3000, 'localhost', (err) => {
   if (err) {
-    console.log(err);
-    return;
+    console.log(err)
+    return
   }
-});
+})
