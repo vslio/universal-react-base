d1f191f8b9cac62ec753bcb4b76788ccb57ce985
Kosta Vasileiou <hello@vsl.io>
2016-04-05 18:22:19 +0100

    Updated API URL to use the remote API server

--- src/shared/actions/index.js
+++ src/shared/actions/index.js
@@ -1,7 +1,7 @@
 /**
  * Generic constants and helper functions for the actions
  */
-const apiUrl = 'http://localhost:3001'
+const apiUrl = 'http://front.the-dots.com:3001'
 
 export function apiPath(path) {
   return apiUrl + path
